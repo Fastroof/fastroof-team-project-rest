e28e72f2f9c239a2598e213a85f7ab5066083911
Fastroof <50989762+Fastroof@users.noreply.github.com>
2022-02-23 22:13:24 +0200

    initial

---
+++ src/test/java/com/fastroof/ftpr/FtprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastroof.ftpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
